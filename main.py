a2dd3e60739176893d16b33ae7a58f36f3fa3721
adeleltaher <adel.eltaher@student.supsi.ch>
2025-08-29 13:56:46 +0200

    recognizes face

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import cv2
+import os
 
 cv2.namedWindow("preview")
 webcam = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier("cv2/data/haarcascade_frontalface_default.xml")
+
+face_classifier = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
 
 if not webcam.isOpened():
     print("Cannot open webcam")
@@ -14,8 +18,17 @@ while True:
         print("Cannot read frame")
         break
 
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_classifier.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=15, minSize=(40,40))
+
+    for face in faces:
+        print(face)
+        x, y, w, h = face
+        cv2.putText(frame, "face", (x, y - 15), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 4)
+
     cv2.imshow("preview", frame)
-    # OpenCV needs a key to quit the window otherwise the winodow is frozen and never shows up
     if cv2.waitKey(1) == ord("q"):
         break
 
